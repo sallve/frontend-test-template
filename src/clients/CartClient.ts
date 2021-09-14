bc7cc28695c23cef5a902297a7796fef8a1b65b3
Erick Saito <saitoerick@gmail.com>
2021-09-14 10:40:13 -0300

    Add backend package and clients

---
+++ src/clients/CartClient.ts
@@ -0,0 +1,3 @@
+import { CartClient } from '@sallve/backend-test-ts';
+
+export default CartClient('http://demotestalb-88047269.us-east-2.elb.amazonaws.com');
---
+++ src/clients/SkuClient.ts
@@ -0,0 +1,3 @@
+import { SkuClient } from '@sallve/backend-test-ts';
+
+export default SkuClient('http://demotestalb-88047269.us-east-2.elb.amazonaws.com');
