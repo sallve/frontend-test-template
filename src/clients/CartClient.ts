2d54ce7de3160dd074e332abbea3745af29bd6ff
Erick Saito <saitoerick@gmail.com>
2021-10-18 17:43:49 -0300

    Remove clients

--- src/clients/CartClient.ts
+++
@@ -1,3 +0,0 @@
-import { CartClient } from '@sallve/backend-test-ts';
-
-export default CartClient('http://demotestalb-88047269.us-east-2.elb.amazonaws.com');
--- src/clients/SkuClient.ts
+++
@@ -1,3 +0,0 @@
-import { SkuClient } from '@sallve/backend-test-ts';
-
-export default SkuClient('http://demotestalb-88047269.us-east-2.elb.amazonaws.com');
